b64e2aebae3b08a5f5699b9d57ee5207fee177b4
toriqriq <distorsiparadoks@gmail.com>
2025-06-03 15:13:25 +0700

    [RIQ] add to

--- src/background/service-worker-register.js
+++ src/background/service-worker-register.js
@@ -7,7 +7,7 @@ const registerServiceWorker = () => {
 
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/service-worker.js")
+        .register("./service-worker.js")
         .then((registration) => {
           console.log("Service Worker registered:", registration);
           resolve(registration);
